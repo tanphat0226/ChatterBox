5695c885591a45f5673f3ab71018e1b657d16634
Tấn Phát <ryanpham0226@gmail.com>
2025-11-21 19:17:00 +0700

    feat: enhance authentication flow with signup and login functionality, add validation and toast notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,6 @@
+import { Loader } from 'lucide-react'
+import { useEffect } from 'react'
+import { Toaster } from 'react-hot-toast'
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
@@ -6,8 +9,6 @@ import ProfilePage from './pages/ProfilePage'
 import SettingsPage from './pages/SettingsPage'
 import SignUpPage from './pages/SignUpPage'
 import { useAuthStore } from './store/useAuthStore'
-import { useEffect } from 'react'
-import { Loader } from 'lucide-react'
 
 const ProtectedRoute = ({ user }) => {
 	if (!user) return <Navigate to='/login' replace />
@@ -39,10 +40,18 @@ const App = () => {
 				</Route>
 
 				{/* Public Routes */}
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
+				<Route
+					path='/signup'
+					element={!authUser ? <SignUpPage /> : <Navigate to='/' />}
+				/>
+				<Route
+					path='/login'
+					element={!authUser ? <LoginPage /> : <Navigate to='/' />}
+				/>
 				<Route path='/settings' element={<SettingsPage />} />
 			</Routes>
+
+			<Toaster />
 		</div>
 	)
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { axiosInstance } from '../libs/axios'
+import toast from 'react-hot-toast'
 
 export const useAuthStore = create((set) => ({
 	authUser: null,
@@ -18,6 +19,42 @@ export const useAuthStore = create((set) => ({
 			set({ authUser: null })
 		} finally {
 			set({ isCheckingAuth: false })
+		}
+	},
+
+	signup: async (data) => {
+		set({ isSigningUp: true })
+		try {
+			const res = await axiosInstance.post('/auth/signup', data)
+			toast.success('Signup successful!')
+			set({ authUser: res.data })
+		} catch (error) {
+			toast.error(error.response?.data?.message || 'Signup failed')
+		} finally {
+			set({ isSigningUp: false })
+		}
+	},
+
+	login: async (data) => {
+		set({ isLoggingIn: true })
+		try {
+			const res = await axiosInstance.post('/auth/login', data)
+			toast.success('Login successful!')
+			set({ authUser: res.data })
+		} catch (error) {
+			toast.error(error.response?.data?.message || 'Login failed')
+		} finally {
+			set({ isLoggingIn: false })
+		}
+	},
+
+	logout: async () => {
+		try {
+			await axiosInstance.post('/auth/logout')
+			set({ authUser: null })
+			toast.success('Logged out successfully')
+		} catch (error) {
+			toast.error(error.response?.data?.message || 'Logout failed')
 		}
 	},
 }))
